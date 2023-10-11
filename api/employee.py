e792a813832450c2e708641c2ee60bd21157cc80
Beau <a02278030@aggies.usu.edu>
2023-10-11 10:49:18 -0600

    drone get function

--- api/employee.py
+++ api/employee.py
@@ -3,18 +3,37 @@ import json
 from flask import (
     Blueprint,
     request,
+    session,
 )
 
 from api.db import get_db
 
 bp = Blueprint("employee", __name__, url_prefix="/employee")
 
-
 @bp.route("/drones", methods=["GET"])
 def drones():
+    owner_id = session.get('user_id')
     db = get_db()
+    error = None
+    query= """
+    SELECT display_name, id, is_active
+    FROM drone
+    WHERE id = ?
+    """ 
+    drones = db.execute(query, owner_id).fetchall()
+
+    if drone is None:
+        error = "This feature is not available yet - check back later!"
+    elif len(drones) == 0:
+        error = "No drones have been registered!"
+
+    if error:
+        return json.dumps({"error": error})
+    else:
+        return json.dumps({"drones": drones})
     # return list of all drones and their immediately relevant info
     # response should look like: {'drones': [ {name: Drone2, size: number, id: 10394825, isActive: true } ]}
+
 
 
 @bp.route("/earnings", methods=["GET"])
