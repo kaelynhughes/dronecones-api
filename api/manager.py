52866d452db149429511e923dead7026d94eab6c
A01972463 <a01972463@usu.edu>
2023-10-13 13:25:01 -0600

    Update manager.py
    Second draft of request handling.

--- api/manager.py
+++ api/manager.py
@@ -36,7 +36,7 @@ def orders():
         return json.dumps({"orders": orders})
 
 
-@bp.route("/product", methods=["GET"])
+@bp.route("/product", methods=["GET", "PUT", "POST"])
 def accounting():
     if request.method == "GET":
         db = get_db()
@@ -62,59 +62,102 @@ def accounting():
             return json.dumps({"item": item})
 
     if request.method == "PUT":
+        # grab data
+        data = request.get_json()
+        displayName = data["display_name"]
+        stock = data["stock"]
+        pricePerUnit = data["price_per_unit"]
+        productType = data["product_type"]
+
+        # initiating variable
         db = get_db()
         error = None
-        query = """
-            SELECT stock
-            FROM product
-            WHERE product_type = {item_to_change}
-            """
-        stock = db.execute(query).fetchone()
 
-        if stock is None:
-            error = "Not implemented"
-        elif len(user) == 0:
-            error = "Empty table"
+        # check data entries
+        if not displayName:
+            error = "Display name missing."
+        elif not stock:
+            error = "Stock missing."
+        elif not pricePerUnit:
+            error = "Price per unit missing."
+        elif not productType:
+            error = "Product type missing."
 
-        if error:
-            return json.dumps({"error": error})
+        # continue if no errors
+        if error is None:
+            try:
+                query = """
+                    REPLACE INTO product (display_name, stock, price_per_unit, product_type)
+                    VALUES (?, ?, ?, ?)
+                    """
+                db.execute(query, (displayName, stock, pricePerUnit, productType))
+                db.commit()
+            except:
+                error = "Error saving values"
+            else:
+                return json.dumps({"success": f"{displayName} has been updated."})
         else:
-            return json.dumps({"stock": {user_input}})
+            return json.dumps({"error": error})
         # update record on one specific product
 
     if request.method == "POST":
-        # add a new product
+        # grab data
+        data = request.get_json()
+        displayName = data["display_name"]
+        stock = data["stock"] if "stock" in data else 0
+        pricePerUnit = data["price_per_unit"]
+        productType = data["product_type"]
+
+        # initiating variable
         db = get_db()
         error = None
-        query = """
-            SELECT *
-            FROM product
-            """
-        menu = db.execute(query).fetchall()
-        if new_item not in menu:
-            menu.append(new_item)
-        else:
-            error = "Item already in menu"
 
-        if error:
-            return json.dumps({"error": error})
+        # check data entries
+        if not displayName:
+            error = "Product name is required."
+        elif not pricePerUnit:
+            error = "Price per unti is required."
+        elif not productType:
+            error = "Product type is required."
+
+        # continue if no errors
+        if error is None:
+            try:
+                query = """
+                    INSERT INTO product (display_name, stock, price_per_unit, product_type)
+                    VALUES (?, ?, ?, ?)
+                    """
+                db.execute(query, (displayName, stock, pricePerUnit, productType))
+                db.commit()
+            except:
+                error = "Error saving product."
+            else:
+                return json.dumps({"success": f"{productType} added successfully."})
         else:
-            return json.dumps({"menu": menu})
+            return json.dumps({"error": error})
 
 
 @bp.route("/user", methods=["GET", "PUT"])
 def user():
     if request.method == "GET":
+        # grab data
+        data = request.get_json
+        search = data["search"]
+
         db = get_db()
-        # get more info about a specific user
         error = None
+
+        if not search:
+            return json.dumps({"error": "Missing search term."})
+
+        # get more info about a specific user
         query = """
-            SELECT username
+            SELECT *
             FROM user
-            WHERE username = '{search term}'    
+            WHERE username = ?  
             """
         # The idea is to find a specific user, right? Hence the {search term}
-        user = db.execute(query).fetchall()
+        user = db.execute(query, (search)).fetchall()
         print(user)
 
         if user is None:
@@ -128,26 +171,40 @@ def user():
             return json.dumps({"user": user})
 
     if request.method == "PUT":
+        # grab data
+        data = request.get_json()
+        username = data["username"]
+        password = data["password"]
+        userType = data["user_type"]
+        isActive = data["is_active"]
+
+        # initiating variable
         db = get_db()
         error = None
-        query = """
-            SELECT is_active
-            FROM user
-            WHERE username = {search_term}
-            """
-        user = db.execute(query).fetchone()
 
-        if user is None:
-            error = "Not implemented"
-        elif len(user) == 0:
-            error = "Empty table"
+        # check data entries
+        if not username:
+            error = "Username missing."
+        elif not password:
+            error = "Password missing."
+        elif not userType:
+            error = "User type missing."
+        elif not isActive:
+            error = "Active value missing."
 
-        # update a user's info - this will be used to ban them
-
-        if error:
-            return json.dumps({"error": error})
-        else:
-            if user.is_active == 0:
-                return json.dumps({"is_active": 1})
+        # continue if no errors
+        if error is None:
+            try:
+                query = """
+                    REPLACE INTO user (username, password, user_type, is_active)
+                    VALUES (?, ?, ?, ?)
+                    """
+                db.execute(query, (username, password, userType, isActive))
+                db.commit()
+            except:
+                error = "Error saving value"
             else:
-                return json.dumps({"is_active": 0})
+                return json.dumps({"success": f"Active value set to {isActive}"})
+        else:
+            return json.dumps({"error": error})
+        # update a user's info - this will be used to ban them
