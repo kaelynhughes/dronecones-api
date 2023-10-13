eb19f08d846c121e841586e87972885f5e8ed803
Beau <a02278030@aggies.usu.edu>
2023-10-12 18:18:01 -0600

    added register drone

--- api/employee.py
+++ api/employee.py
@@ -10,16 +10,18 @@ from api.db import get_db
 
 bp = Blueprint("employee", __name__, url_prefix="/employee")
 
+
 @bp.route("/drones", methods=["GET"])
 def drones():
-    owner_id = session.get('user_id')
+    owner_id = 1
+    # session.get("user_id")
     db = get_db()
     error = None
-    query= """
+    query = """
     SELECT display_name, id, is_active
     FROM drone
     WHERE id = ?
-    """ 
+    """
     drones = db.execute(query, owner_id).fetchall()
 
     if drone is None:
@@ -35,7 +37,6 @@ def drones():
     # response should look like: {'drones': [ {name: Drone2, size: number, id: 10394825, isActive: true } ]}
 
 
-
 @bp.route("/earnings", methods=["GET"])
 def earnings():
     db = get_db()
@@ -46,8 +47,33 @@ def earnings():
 @bp.route("/drone", methods=["POST", "DELETE", "PUT"])
 def drone():
     if request.method == "POST":
+        print(request.form["display_name"])
+        display_name = request.form["display_name"]
+        drone_size = request.form["drone_size"]
+        owner_id = 1
+        # session.get("user_id")
         db = get_db()
+        error = None
+
+        if not display_name:
+            error = "Display Name is required."
+        elif not drone_size:
+            error = "Drone Size is required."
         # register a drone
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO drone (display_name, drone_size, owner_id) VALUES (?, ?, ?)",
+                    (display_name, drone_size, owner_id),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"Drone {display_name} is already registered."
+            else:
+                return json.dumps({"success": display_name})
+        # if we don't end up being able to register, return the error
+        return json.dumps({"error": error})
 
     if request.method == "DELETE":
         db = get_db()
