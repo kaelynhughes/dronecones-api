c670b28f961d3370e777024320eeb47198ff0c8c
Kaelyn Hughes <kaelynmhughes@gmail.com>
2023-11-29 14:53:48 -0700

    handle malformed requests gracefully

--- api/auth.py
+++ api/auth.py
@@ -27,33 +27,40 @@ bp = Blueprint("auth", __name__, url_prefix="/auth")
 def login():
     # request.form is a kind of dict mapping with form keys and values
     body = request.get_json()
+    try:
+        username = body["username"]
+        password = body["password"]
+    except KeyError:
+        return {"error": "Bad data: username or password not provided."}, 400
 
-    username = body["username"]
-    password = body["password"]
     db = get_db()
-    error = None
 
     # execute does an SQL query
     # fetchone returns one row, as opposed to fetchall which fetches the first matching row
     user = db.execute("SELECT * FROM user WHERE username = ?", (username,)).fetchone()
 
     if user is None:
-        error = "Incorrect username."
+        return {"error": "Incorrect username."}, 401
     elif not check_password_hash(user["password"], password):
-        error = "Incorrect password."
+        return {"error": "Incorrect password."}, 401
 
-    if error is None:
-        session.clear()
+    session.clear()
+    try:
         session["user_id"] = user["id"]
         user_type = user["user_type"]
         user_id = user["id"]
         active = user["is_active"]
+    except Exception:
+        return {
+            "error": "Looks like there's some malformed data on our end. Sorry!"
+        }, 404
 
     if not active:
-        error = "This user has been deactivated.  Please contact us to appeal this ban."
+        return {
+            "error": "This user has been deactivated.  Please contact us to appeal this ban."
+        }, 401
 
-    flash(error)
-    response = {"error": error} if error else {"id": user_id, "user_type": user_type, "is_active": active}
+    response = {"id": user_id, "user_type": user_type, "is_active": active}
     return json.dumps(response)
 
 
@@ -61,26 +68,23 @@ def login():
 @bp.route("/register", methods=["POST"])
 def register():
     body = request.get_json()
-
-    username = body["username"]
-    password = body["password"]
-    user_type = body["user_type"]
+    try:
+        username = body["username"]
+        password = body["password"]
+        user_type = body["user_type"]
+    except KeyError:
+        return {
+            "error": "Bad data: username, password, or user type not provided."
+        }, 401
     db = get_db()
-    error = None
 
-    if not username:
-        error = "Username is required."
-    elif not password:
-        error = "Password is required."
-
-    if error is None:
-        try:
-            id = db.execute(
-                "INSERT INTO user (username, password, user_type, is_active) VALUES (?, ?, ?, ?)",
-                (username, generate_password_hash(password), user_type, 1),
-            ).lastrowid
-            db.commit()
-        except db.IntegrityError:
-            error = f"User {username} is already registered."
-    response = {"error": error} if error else {"success": id}
-    return json.dumps(response)
+    try:
+        id = db.execute(
+            "INSERT INTO user (username, password, user_type, is_active) VALUES (?, ?, ?, ?)",
+            (username, generate_password_hash(password), user_type, 1),
+        ).lastrowid
+        db.commit()
+    except db.IntegrityError:
+        return {"error": f"User {username} is already registered."}, 409
+    response = {"success": id}
+    return json.dumps(response, 201)
