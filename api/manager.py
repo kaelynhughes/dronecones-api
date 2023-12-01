bc63b12270eaf5cabd7423550a8638e44fe1b04a
Kaelyn Hughes <kaelynmhughes@gmail.com>
2023-11-30 23:58:12 -0700

    /orders (all), /users (all), & /product (get, put) now have correct error handling

--- api/manager.py
+++ api/manager.py
@@ -19,7 +19,7 @@ def orders():
                 FROM full_order
                 ORDER BY id DESC
                 LIMIT 50
-                """ 
+                """
         orders = db.execute(query).fetchall()
         full_order_dict = [dict(row) for row in orders]
 
@@ -28,6 +28,7 @@ def orders():
         # if we want to get only a few at a time, we may need to have this be a bit more involved
         # so that we don't end up returning the same few records over and over
         # this will be used in account management as well
+
 
 @bp.route("/users", methods=["GET"])
 def users():
@@ -41,14 +42,13 @@ def users():
     users = [dict(row) for row in usersResp]
 
     if users is None:
-        error = "No users were found"
-    
-    if error is None:
-            return json.dumps(users)
-    return json.dumps(error)
+        return {" error": "No users were found"}  # , 404
+
+    return json.dumps(users)
+
 
 @bp.route("/product", methods=["GET", "PUT", "POST", "DELETE"])
-def accounting(): #does this need to be product()
+def accounting():  # does this need to be product()
     if request.method == "GET":
         db = get_db()
         body = request.get_json()
@@ -57,7 +57,7 @@ def accounting(): #does this need to be product()
         id = body["id"]
 
         if not id or type(id) is not int:
-            error = "Id is required and must be a int."
+            return {"error": "Id is required and must be an integer."}
 
         query = """
         SELECT *
@@ -66,65 +66,127 @@ def accounting(): #does this need to be product()
         """
         product = db.execute(query, (id,)).fetchall()
 
-        if not error:
-            if product is None:
-                error = "This feature is not available yet - check back later!"
-            elif len(product) == 0:
-                error = f"No product has been registered with id {id}"
-            else:
-                product_dict = [dict(row) for row in product]
+        if product is None:
+            return {"error": "This feature is not available yet - check back later!"}
 
-        if error:
-            return json.dumps({"error": error})
+        if len(product) == 0:
+            return {"error": f"No product has been registered with id {id}"}
         else:
-            return json.dumps({"product": product_dict})
-    
+            product_dict = [dict(row) for row in product]
+
+        return json.dumps({"product": product_dict})
+
         # get info on one specific product
 
-    if request.method == "PUT": #not tested
+    # in 2.0, make this its own function that takes a product ID in the query string
+    if request.method == "PUT":  # not tested
         db = get_db()
         body = request.get_json()
-
+        if "id" not in body:
+            return {"error": "We need a product ID to update your product!"}  # , 400
         id = body["id"]
-        display_name = body["display_name"]
-        stock = body["stock"]
-        price_per_unit = body["price_per_unit"]
 
-        if not id or type(id) is not int:
-            error = "Id is required and must be a integer."
-        elif not display_name or type(display_name) is not str:
-            error = "Display Name is required and must be a string."
-        elif not stock or type(stock) is not int:
-            error = "Stock is required and must be a integer."
-        elif not price_per_unit or type(price_per_unit) is not int:
-            error = "Price Per Unit is required and must be a integer."
+        if type(id) is not int:
+            return {"error": "ID must be an integer."}  # , 400
 
+        try:
+            query = """
+                SELECT *
+                FROM product 
+                WHERE id = ?
+                """
+            existing_product = db.execute(query, (id,)).fetchone()
+            # this is janky. change in 2.0 so we don't necessarily return stock
+            if not existing_product:
+                return {
+                    "error": f"Couldn't find product with ID {id} because it does not exist."
+                }  # , 400
+            stock = existing_product["stock"]
+        except db.IntegrityError:
+            return {"error": f"We couldn't find a product with the ID {id}."}
 
-        query = """
-            SELECT *
-            FROM product 
-            WHERE id = ?
-            """
-
-        existing_product = db.execute(query, (id,)).fetchone()
-        if not existing_product:
-            error = f"Couldn't find stock with id {id} because it does not exist."
-        else:
+        if "display_name" in body:
+            display_name = body["display_name"]
+            if type(display_name) is not str:
+                return {"error": "Display name must be a string."}  # , 400
             try:
                 query = """
                     UPDATE product
-                    SET display_name = ?, stock = ?, price_per_unit = ?
+                    SET display_name = ?
                     WHERE id = ?
                     """
                 db.execute(
                     query,
-                    (display_name, stock, price_per_unit, id),
+                    (display_name, id),
                 )
                 db.commit()
-                return json.dumps({"Updated stock with id ": stock})
-            except db.IntegrityError:
-                error = f"An error occurred while updating the stock with id {id}."
-        return json.dumps({"error": error})
+            except db.IntegrityError as ex:
+                print(ex)
+                return {
+                    "error": "An unexpected database error occurred on our end while updating display name. Sorry!"
+                }  # , 500
+            except Exception as ex:
+                print(ex)
+                return {
+                    "error": "An unexpected error occurred on our end while updating display name. Sorry!"
+                }  # , 500
+
+        if "stock" in body:
+            stock = body["stock"]
+            if type(stock) is not int:
+                return {"error": "Stock must be a integer."}  # , 400
+            try:
+                query = """
+                    UPDATE product
+                    SET stock = ?
+                    WHERE id = ?
+                    """
+                db.execute(
+                    query,
+                    (stock, id),
+                )
+                db.commit()
+            except db.IntegrityError as ex:
+                print(ex)
+                return {
+                    "error": "An unexpected database error occurred on our end while updating stock. Sorry!"
+                }  # , 500
+            except Exception as ex:
+                print(ex)
+                return {
+                    "error": "An unexpected error occurred on our end while updating stock. Sorry!"
+                }  # , 500
+
+        if "price_per_unit" in body:
+            price_per_unit = body["price_per_unit"]
+            if type(price_per_unit) is not int:
+                return {"error": "Price per unit must be a integer."}  # , 400
+            try:
+                query = """
+                    UPDATE product
+                    SET price_per_unit = ?
+                    WHERE id = ?
+                    """
+                db.execute(
+                    query,
+                    (price_per_unit, id),
+                )
+                db.commit()
+            except db.IntegrityError as ex:
+                print(ex)
+                return {
+                    "error": "An unexpected database error occurred on our end while updating price per unit. Sorry!"
+                }  # , 500
+            except Exception as ex:
+                print(ex)
+                return {
+                    "error": "An unexpected error occurred on our end while updating price per unit. Sorry!"
+                }  # , 500
+
+        # JANKY ALERT in 2.0 this should be updated to something like {"success": message} where message tells what was updated
+        # also let it be known i find this return statement incredibly funny
+        return json.dumps({"Updated stock with id ": stock})
+
         # update record on one specific product - can be used for restock
 
     if request.method == "POST":
@@ -149,13 +211,15 @@ def accounting(): #does this need to be product()
                     INSERT INTO product (display_name, stock, price_per_unit, product_type)
                     VALUES (?, ?, ?, ?)
                     """
-                id = db.execute(query, (display_name, stock, price_per_unit, product_type)).lastrowid
+                id = db.execute(
+                    query, (display_name, stock, price_per_unit, product_type)
+                ).lastrowid
                 db.commit()
             except db.IntegrityError:
                 error = "Sorry, something went wrong saving the product."
             else:
                 return json.dumps({"success": id})
-            
+
     if request.method == "DELETE":
         db = get_db()
         error = None
@@ -181,9 +245,7 @@ def accounting(): #does this need to be product()
                     DELETE FROM product
                     WHERE id = ?
                     """
-                db.execute(
-                    query, (id,)
-                )
+                db.execute(query, (id,))
                 db.commit()
             except db.IntegrityError:
                 error = f"An error occurred while deleting the product with id {id}"
@@ -253,16 +315,14 @@ def user():
                     SET is_active = ?
                     WHERE id = ?
                     """
-                db.execute(
-                    query,
-                    (is_active, id)
-                )
+                db.execute(query, (is_active, id))
                 db.commit()
                 return json.dumps({"Updated user with id ": id})
             except db.IntegrityError:
                 error = f"An error occurred while updating the user with id {id}."
         return json.dumps({"error": error})
         # update a user's info - this will be used to ban them
+
 
 @bp.route("/history", methods=["GET"])
 def history():
@@ -278,10 +338,9 @@ def history():
                 LIMIT 50
                 """
         orders = db.execute(query).fetchall()
-        
 
         if orders is None:
-                error = "This feature is not available yet - check back later!"
+            error = "This feature is not available yet - check back later!"
         else:
             full_order_dict = [dict(row) for row in orders]
             for order in full_order_dict:
@@ -290,7 +349,7 @@ def history():
                         SELECT *
                         FROM ordered_cone
                         WHERE order_id = ?
-                        """ 
+                        """
                 ordered_cones = db.execute(query, (full_order_id,)).fetchall()
 
                 if ordered_cones is None:
