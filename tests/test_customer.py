19779d66ab3ed13c24c61bed8c3e5bcef5b4771a
beau <a02278030@aggies.usu.edu>
2023-12-03 14:00:34 -0700

    finihed tests for customer

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -23,11 +23,11 @@ def test_menu(client, app):
     assert len(menu["icecream"]) == 3
 
 
-def test_checkout_get(client):
+def test_checkout_post_get(client):
     # Test the /customer/<customer_id>/checkout GET endpoint
     response = client.get("/customer/1/checkout")
     assert response.status_code == 200
-    assert "error" in response.json
+    assert "error" in json.loads(response.data)
     data = {
         "total_price": 500,
         "employee_cut": 100,
@@ -59,40 +59,10 @@ def test_checkout_get(client):
     data = json.loads(response.data)
     assert "full_order_id" in data
 
-
-
-def test_checkout_post(client):
-    # Test the /customer/<customer_id>/checkout POST endpoint
-    data = {
-        "total_price": 500,
-        "employee_cut": 100,
-        "profit": 400,
-        "order_time": "2023-11-19 10:30:00",
-        "cones": [
-            {
-                "cone": 1,
-                "scoop_1": 7,
-                "scoop_2": 8,
-                "scoop_3": 9,
-                "topping_1": 4,
-                "topping_2": 5,
-                "topping_3": 6
-            },
-            {
-                "cone": 2,
-                "scoop_1": 8,
-                "scoop_2": 9,
-                "scoop_3": 7,
-                "topping_1": 5,
-                "topping_2": 6,
-                "topping_3": 4
-            }
-        ]
-    }
-    response = client.post("/customer/1/checkout", json=data)
+    response = client.get("/customer/1/checkout")
     assert response.status_code == 200
     data = json.loads(response.data)
-    assert "full_order_id" in data
+    assert "ordered cone" in data
 
 
 def test_history(client, app):
@@ -101,14 +71,7 @@ def test_history(client, app):
     data = json.loads(response.data)
     assert "orders_history" in data
 
-def test_account_get(client):
-    # Test the /customer/<customer_id>/account GET endpoint
-    response = client.get("/customer/1/account")
-    assert response.status_code == 200
-    data = json.loads(response.data)
-    assert "customer" in data
-
-def test_account_put(client):
+def test_account_get_put(client):
     # Test the /customer/<customer_id>/account PUT endpoint
     data = {
         "username": "new_username",
@@ -119,3 +82,9 @@ def test_account_put(client):
     assert response.status_code == 200
     data = json.loads(response.data)
     assert "success" in data
+
+    # Test the /customer/<customer_id>/account GET endpoint
+    response = client.get("/customer/1/account")
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert "new_username" in data["customer"][0]["username"]
